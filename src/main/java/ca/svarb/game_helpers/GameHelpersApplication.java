42fc4fa5ffb5c3c4b6f51423a0e37b406bdad1a3
Bhadresh Modi <bmodi@crystalfountains.com>
2025-01-02 02:18:44 -0500

    Added REST API endpoint

--- src/main/java/ca/svarb/game_helpers/GameHelpersApplication.java
+++ src/main/java/ca/svarb/game_helpers/GameHelpersApplication.java
@@ -3,7 +3,7 @@ package ca.svarb.game_helpers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"ca.svarb.whelper", "ca.svarb.whelper.rest"})
 public class GameHelpersApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/ca/svarb/whelper/rest/GridService.java
@@ -0,0 +1,60 @@
+package ca.svarb.whelper.rest;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.SortedSet;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import ca.svarb.whelper.Dictionary;
+import ca.svarb.whelper.DictionaryLoader;
+import ca.svarb.whelper.IGameBoard;
+import ca.svarb.whelper.WordSearcher;
+import ca.svarb.whelper.WhelperException;
+
+@RestController
+@RequestMapping("/grid")
+public class GridService {
+
+    private Dictionary dictionary = null;
+
+    @Autowired
+    private WordSearcher wordSearcher;
+
+    public GridService() {
+        try {
+            loadDefaultDictionary();
+        } catch (WhelperException e) {
+            System.out.println("Could not load dictionary: " + e.getMessage());
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public SortedSet<String> getWords(@RequestBody Grid grid) {
+        System.out.println("grid=" + grid);
+        IGameBoard board = null;
+        if (grid.getGridType() == Grid.GridType.GRID) {
+            board = new ca.svarb.whelper.Grid(grid.getCells());
+        } else {
+            board = new ca.svarb.whelper.OffsetGrid(grid.getCells());
+        }
+        return wordSearcher.findWords(dictionary, board);
+    }
+
+    private void loadDefaultDictionary() throws WhelperException {
+        String dictionaryName = "TWL06.txt";
+
+        try {
+            InputStream dictionaryStream = GridService.class.getClassLoader().getResourceAsStream(dictionaryName);
+            if (dictionaryStream == null) throw new WhelperException("Could not find dictionary file: " + dictionaryName);
+            dictionary = DictionaryLoader.getInstance().loadFromReader(new InputStreamReader(dictionaryStream));
+        } catch (IOException e) {
+            throw new WhelperException("Error reading from dictionary file: " + e.getMessage());
+        }
+    }
+}
